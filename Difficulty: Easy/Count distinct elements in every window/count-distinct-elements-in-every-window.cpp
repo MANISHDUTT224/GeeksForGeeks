9f82349abb492c1461794c3f42f02984ba9b5f3f
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-10 16:38:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count distinct elements in every window/count-distinct-elements-in-every-window.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
+        // code here.
+       unordered_map<int,int>mp;
+       vector<int>res;
+       int n=arr.size(),l=0,r=0;
+       while(r<n){
+           mp[arr[r]]++;
+           if(r-l+1==k){
+               int sz=mp.size();
+               res.push_back(sz);
+               mp[arr[l]]--;
+               if(mp[arr[l]]==0){
+                   mp.erase(arr[l]);
+               }
+               l++;
+           }
+           r++;
+       }
+       return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        vector<int> res = obj.countDistinct(arr, k);
+        for (auto it : res)
+            cout << it << " ";
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
