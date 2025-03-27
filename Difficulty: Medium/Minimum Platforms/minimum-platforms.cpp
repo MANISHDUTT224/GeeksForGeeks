cdecc59e818104077e058deef401a07d048c0c6c
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-27 15:58:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // Your code here
+        int n=arr.size();
+        vector<int>time(2361,0);
+        for(int i=0;i<n;i++){
+            time[arr[i]]++;
+            time[dep[i]+1]--;
+        }
+        int maxcnt=0,sumcnt=0;
+        for(int i=0;i<2361;i++){
+            sumcnt+=time[i];
+            maxcnt=max(maxcnt,sumcnt);
+        }
+        return maxcnt;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, dep;
+        string input;
+
+        // Read first array (arr)
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Read second array (dep)
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            dep.push_back(number);
+        }
+
+        // Assuming Solution class is defined elsewhere with a method findPlatform
+        Solution ob;
+        cout << ob.findPlatform(arr, dep);
+
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
