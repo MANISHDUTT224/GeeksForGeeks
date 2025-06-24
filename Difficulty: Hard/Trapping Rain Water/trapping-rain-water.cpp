f3cce20236caa9849265b183e03ebad1b00ec945
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-25 01:41:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -1,16 +1,11 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     int maxWater(vector<int> &arr) {
         // code here
-        int n=arr.size(),trapped=0;
-        int left[n],right[n];
+        int n=arr.size();
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        int trapped=0;
         left[0]=arr[0],right[n-1]=arr[n-1];
         for(int i=1;i<n;i++){
             left[i]=max(left[i-1],arr[i]);
@@ -25,30 +20,3 @@ class Solution {
         return trapped;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
