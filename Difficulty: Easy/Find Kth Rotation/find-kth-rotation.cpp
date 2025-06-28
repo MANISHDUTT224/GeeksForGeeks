44f1dc4afa8d236d4f2fb8f6e8f90a37579dd30f
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-28 13:10:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -1,39 +1,30 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function template for C++
 class Solution {
   public:
-    int findKRotation(vector<int> &arr) {
+    int findKRotation(vector<int> &nums) {
         // Code Here
-        int i=0,j=arr.size()-1;
+        int i=0,j=nums.size()-1,ans=INT_MAX;
         int index=0;
-        int ans=INT_MAX;
         while(i<=j){
             int mid=(i+j)/2;
-            if(arr[i]<=arr[j]){
-                if(arr[i]<ans){
+            if(nums[i]<=nums[j]){
+                if(nums[i]<ans){
                     index=i;
-                    ans=arr[i];
+                    ans=nums[i];
                 }
                 break;
             }
-            if(arr[i]<=arr[mid]){
-                if(arr[i]<ans){
+            if(nums[i]<=nums[mid]){
+                if(nums[i]<ans){
                     index=i;
-                    ans=arr[i];
+                    ans=nums[i];
                 }
                 i=mid+1;
             }
             else{
-                if(arr[mid]<ans){
+                if(nums[mid]<ans){
                     index=mid;
-                    ans=arr[mid];
+                    ans=nums[mid];
                 }
                 j=mid-1;
             }
@@ -41,29 +32,3 @@ class Solution {
         return index;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input1;
-        getline(cin, input1);
-        stringstream ss1(input1);
-        int number1;
-        while (ss1 >> number1) {
-            arr.push_back(number1);
-        }
-        Solution ob;
-        int res = ob.findKRotation(arr);
-        cout << res << endl;
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
