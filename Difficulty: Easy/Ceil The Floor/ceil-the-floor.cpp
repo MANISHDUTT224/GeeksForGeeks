3f03920b63ec846a005ab8ee0302b5eee069d5c4
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-28 12:39:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
@@ -1,62 +1,33 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User code template
 
 class Solution {
   public:
     vector<int> getFloorAndCeil(int x, vector<int> &arr) {
         // code here
-        vector<int>res={INT_MIN,INT_MAX};
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]>=x && res[1]>arr[i]){
-                res[1]=arr[i];
+       int first=-1,last=-1;
+        sort(arr.begin(),arr.end());
+        int i=0,j=arr.size()-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(arr[mid]>=x){
+                last=arr[mid];
+                j=mid-1;
             }
-            if(arr[i]<=x && res[0]<arr[i]){
-                res[0]=arr[i];
+            else{
+                i=mid+1;
             }
         }
-        if(res[0]==INT_MIN){
-            res[0]=-1;
+        i=0,j=arr.size()-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(arr[mid]<=x){
+                first=arr[mid];
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
         }
-        if(res[1]==INT_MAX){
-            res[1]=-1;
-        }
-        return res;
+        return {first,last};
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after t
-    while (t--) {
-        vector<int> arr;
-        int x;
-        string input;
-        cin >> x;
-        cin.ignore();
-
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        auto ans = ob.getFloorAndCeil(x, arr);
-        cout << ans[0] << " " << ans[1] << "\n~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
