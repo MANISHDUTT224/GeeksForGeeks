7e221e3afa2b073a896fb3a2c3521109fcaa6627
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-08 22:21:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find nth root of m/find-nth-root-of-m.cpp
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.cpp
@@ -1,34 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int solve(int mid,int n,int m){
-        long long ans=1;
-        for(int i=1;i<=n;i++){
-            ans*=mid;
-            if(ans>m){
-                return 2;
-            }
-        }
-        if(ans==m){
-            return 1;
-        }
-        return 0;
-    }
+  int func(int mid,int n,int m){
+      int ans=1;
+      for(int i=1;i<=n;i++){
+          ans*=mid;
+          if(ans>m){
+              return 2;
+          }
+      }
+      if(ans==m){
+          return 1;
+      }
+      return 0;
+  }
     int nthRoot(int n, int m) {
         // Code here.
         int i=1,j=m;
         while(i<=j){
             int mid=(i+j)/2;
-            if(solve(mid,n,m)==1){
+            int midN=func(mid,n,m);
+            if(midN==1){
                 return mid;
             }
-            else if(solve(mid,n,m)==2){
+            else if(midN==2){
                 j=mid-1;
             }
             else{
@@ -38,22 +32,3 @@ class Solution {
         return -1;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        Solution ob;
-        int ans = ob.nthRoot(n, m);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
