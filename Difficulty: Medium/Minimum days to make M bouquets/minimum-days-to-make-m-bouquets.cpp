1debf75719290e84fd9bda62d2cc372e03e5a66b
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-08 22:30:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum days to make M bouquets/minimum-days-to-make-m-bouquets.cpp
+++ Difficulty: Medium/Minimum days to make M bouquets/minimum-days-to-make-m-bouquets.cpp
@@ -1,34 +1,33 @@
 class Solution {
   public:
-     bool Possible(vector<int>&arr,int mid,int m,int k){
-         int bouqets=0,conlen=0;
-         for(int i=0;i<arr.size();i++){
-             if(arr[i]<=mid){
-                 conlen++;
-             }
-             else{
-                 bouqets+=conlen/k;
-                 conlen=0;
-             }
-         }
-         bouqets+=conlen/k;
-         return bouqets>=m;
-     }
+    bool isPossible(vector<int>&arr,int mid,int m,int k){
+        int conlen=0,bouqets=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=mid){
+                conlen++;
+            }
+            else{
+                bouqets+=conlen/k;
+                conlen=0;
+            }
+        }
+        bouqets+=conlen/k;
+        return bouqets>=m;
+    }
     int minDaysBloom(int m, int k, vector<int> &arr) {
         // Code here
         sort(arr.begin(),arr.end());
-        int ans=-1;
-        int i=*min_element(arr.begin(),arr.end());
-        int j=*max_element(arr.begin(),arr.end());
-        long long val=k*1LL*m*1LL;
+        int i=*min_element(arr.begin(),arr.end()),j=*max_element(arr.begin(),arr.end());
+        long long val=m*1LL*k*1LL;
         if(val>arr.size()){
             return -1;
         }
+        int ans=-1;
         while(i<=j){
             int mid=(i+j)/2;
-            if(Possible(arr,mid,m,k)){
-                ans=mid;
+            if(isPossible(arr,mid,m,k)){
                 j=mid-1;
+                ans=mid;
             }
             else{
                 i=mid+1;
