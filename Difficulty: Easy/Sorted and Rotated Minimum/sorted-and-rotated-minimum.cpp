74a9c7bc31905276958bb3e88ec84ccbe1bafbdb
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-08 20:45:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
@@ -1,23 +1,20 @@
 class Solution {
   public:
-    int findMin(vector<int>& nums) {
+    int findMin(vector<int>& arr) {
         // complete the function here
-        int n=nums.size();
-        int ans=INT_MAX;
+        int n=arr.size(),ans=INT_MAX;
         int i=0,j=n-1;
         while(i<=j){
             int mid=(i+j)/2;
-            if(nums[i]<=nums[j]){
-                ans=min(ans,nums[i]);
-                break;
+            if(arr[i]<=arr[j]){
+                ans=min(ans,arr[i]);
             }
-            if(nums[i]<=nums[mid]){
-                
-                ans=min(ans,nums[i]);
-               i=mid+1;
+            if(arr[i]<=arr[mid]){
+                ans=min(ans,arr[i]);
+                i=mid+1;
             }
             else{
-                ans=min(ans,nums[mid]);
+                ans=min(ans,arr[mid]);
                 j=mid-1;
             }
         }
