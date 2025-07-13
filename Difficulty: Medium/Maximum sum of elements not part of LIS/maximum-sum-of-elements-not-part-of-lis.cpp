32939517a6cd33e9ae4a7231e7d5b9068101ad7b
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-13 08:21:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum sum of elements not part of LIS/maximum-sum-of-elements-not-part-of-lis.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int nonLisMaxSum(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int>lis,ins(n);
+        long long totalSum=0;
+        for(int i=0;i<n;i++){
+            totalSum+=arr[i];
+            auto lb=lower_bound(lis.begin(),lis.end(),arr[i]);
+            if(lb==lis.end()){
+                lis.push_back(arr[i]);
+                ins[i]=lis.size()-1;
+            }
+            else{
+                *lb=arr[i];
+                ins[i]=lb-lis.begin();
+            }
+        }
+        int lisSum=0,ind=lis.size()-1;
+        for(int i=n-1;i>=0;i--){
+            if(ins[i]==ind){
+                lisSum+=arr[i];
+                ind--;
+            }
+        }
+        return totalSum-lisSum;
+    }
+};
