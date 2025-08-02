6b2eda87c866386c08133400be13f68de7f82525
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-08-02 14:32:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    string reverseWords(string &s) {
+        // code here
+     int n=s.size();
+     int i=0;
+     string res;
+     while(i<n){
+         while(i<n && s[i]=='.'){
+             i++;
+         }
+         if(i==n){
+             return res;
+         }
+         int j=i+1;
+         while(j<n && s[j]!='.'){
+             j++;
+         }
+         string sub=s.substr(i,j-i);
+         if(res.size()==0){
+             res=sub;
+         }
+         else{
+             res=sub+"."+res;
+         }
+         i=j+1;
+     }
+     return res;
+     
+    }
+};
