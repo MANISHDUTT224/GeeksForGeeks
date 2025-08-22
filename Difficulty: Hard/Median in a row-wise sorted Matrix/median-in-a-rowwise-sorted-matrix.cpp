a40d9a4b960f3b2275ccdccfc6a9b75579d92a37
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-08-22 14:18:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,49 +1,14 @@
-// User function template for C++
-
 class Solution {
   public:
-    int lowerbound(vector<int>arr,int mi,int m){
-        int i=0,j=m-1;
-        int ans=m;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            if(arr[mid]>mi){
-                ans=mid;
-                j=mid-1;
-            }
-            else{
-                i=mid+1;
-            }
-        }
-        
-        return ans;
-    }
-    int cont(vector<vector<int>>&mat,int mid,int n,int m){
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            cnt+=lowerbound(mat[i],mid,m);
-        }
-        return cnt;
-    }
     int median(vector<vector<int>> &mat) {
         // code here
-        int n=mat.size(),m=mat[0].size();
-        int req=(n*m)/2;
-        int low=INT_MAX,high=INT_MIN;
-        for(int i=0;i<n;i++){
-            low=min(low,mat[i][0]);
-            high=max(high,mat[i][m-1]);
-        }
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int se=cont(mat,mid,n,m);
-            if(se<=req){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+        vector<int> temp;
+        for(int i=0;i<mat.size();i++) {
+            for(int j=0;j<mat[0].size();j++) {
+                temp.push_back(mat[i][j]);
             }
         }
-        return low;
+        sort(temp.begin(),temp.end());
+        return temp[int(mat.size()*mat[0].size())/2];
     }
 };
