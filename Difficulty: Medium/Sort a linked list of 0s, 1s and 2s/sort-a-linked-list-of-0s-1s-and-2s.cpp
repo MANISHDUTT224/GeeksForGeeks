fc7624dda1d9fa6795c1acb4805f5427847c94ca
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-09-05 09:03:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -1,9 +1,5 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Link list Node */
-class Node {
+/* Node is defined as
+  class Node {
   public:
     int data;
     Node* next;
@@ -13,104 +9,23 @@ class Node {
         next = nullptr;
     }
 };
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-void freeList(struct Node* head) {
-    struct Node* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        delete temp;
-    }
-}
-
-
-// } Driver Code Ends
-
-/*  Node is defined as
-  struct Node {
-    int data;
-    struct Node *next;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};*/
+*/
 class Solution {
   public:
+   Node* split(Node *p,int val){
+       Node *q=p;
+       while(p){
+           if(p->data==val){
+               swap(p->data,q->data);
+               q=q->next;
+           }
+           p=p->next;
+       }
+       return q;
+   }
     Node* segregate(Node* head) {
         // code here
-         int count[3] = {0};
-         Node* curr = head;
-
-
-    while (curr) {
-        count[curr->data]++;
-        curr = curr->next;
-    }
-
-    
-    curr = head;
-    int i = 0;
-    while (curr) {
-        if (count[i] == 0) {
-            i++;
-        } else {
-            curr->data = i;
-            count[i]--;
-            curr = curr->next;
-        }
-    }
+        split(split(head,0),1);
     return head;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-// Driver program to test above functions
-int main() {
-    int t, k;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after t
-
-    while (t--) {
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-
-        stringstream ss(input);
-        Node *head = nullptr, *tail = nullptr;
-        int x;
-
-        if (ss >> x) {
-            head = new Node(x);
-            tail = head;
-
-            while (ss >> x) {
-                tail->next = new Node(x);
-                tail = tail->next;
-            }
-
-            // Link the last node to the head if k is 1 to make it circular
-            if (k == 1) {
-                tail->next = head;
-            }
-        }
-
-        Solution ob;
-        Node* newHead = ob.segregate(head);
-        printList(newHead);
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
