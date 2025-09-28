c0dfc0a07bfd0792e3b408321b7956456c4240a7
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-09-28 06:44:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
@@ -1,67 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> longestSubarray(vector<int>& arr, int x) {
         // code here
-        multiset<int>st;
-        int n=arr.size();
-        int l=0,r=0;
-        int ansl=-1,ansr=-1,maxlen=0;
-        while(l<n && r<n){
-            st.insert(arr[r]);
-            while(!st.empty() && abs((*st.begin())-*prev(st.end()))>x){
-                st.erase(st.find(arr[l]));
-                l++;
+        int i=0,j=0,ans=0;
+        multiset<int>set;
+        int l=0,h=0;
+        while(i<arr.size()){
+            set.insert(arr[i]);
+            while(*set.rbegin()-*set.begin()>x){
+                set.erase(set.find(arr[j]));
+                j++;
             }
-            if(st.size()>maxlen){
-                maxlen=st.size();
-                ansl=l;
-                ansr=r;
+            if(ans<i-j+1){
+                l=j;
+                h=i;
+                ans=i-j+1;
             }
-            r++;
+            i++;
         }
-        vector<int>ans;
-        for(int i=ansl;i<=ansr;i++){
-            ans.push_back(arr[i]);
+        vector<int>s;
+        for(int i=l;i<=h;i++){
+            s.push_back(arr[i]);
         }
-        return ans;
+        return s;
         
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.longestSubarray(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
