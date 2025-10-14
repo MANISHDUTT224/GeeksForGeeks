2071b61ce2682d3243d62353587cafdcc27f78f8
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-10-14 13:31:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Nodes in BST Range/sum-of-nodes-in-bst-range.java
@@ -0,0 +1,21 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    int sum=0;
+    public int nodeSum(Node root, int l, int r) {
+     if(root==null) return sum; 
+     nodeSum(root.left,l,r);
+     if(root.data>=l&&root.data<=r) sum+=root.data;
+     nodeSum(root.right,l,r);
+     return sum;
+    }
+}
