9645bd13050a2457dfad112bc0b39b244e271f53
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-01-10 09:42:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -1,28 +1,43 @@
 class Solution {
   public:
-  int f(string &s,int k){
-      int l=0,r=0,cnt=0,distinct=0;
-      int n=s.size();
-      vector<int>mp(26,0);
-      while(r<n){
-          mp[s[r]-'a']++;
-          if(mp[s[r]-'a']==1){
-              distinct++;
-          }
-          while(l<n && distinct>k){
-              mp[s[l]-'a']--;
-              if(mp[s[l]-'a']==0){
-                  distinct--;
-              }
-              l++;
-          }
-          r++;
-          cnt+=(r-l)+1;
-      }
-      return cnt;
-  }
+    int solve(string& str,int k){
+        int n=str.length();
+        
+        vector<int> mp(26,0);
+        
+        int l=0;
+        int r=0;
+        int cnt=0;
+        int ans=0;
+        
+        while(r<n){
+            mp[str[r]-'a']++;
+            
+            if(mp[str[r]-'a']==1){
+                cnt++;
+            }
+            
+            while(cnt>k){
+                mp[str[l]-'a']--;
+                if(mp[str[l]-'a']==0){
+                    cnt--;
+                }
+                l++;
+            }
+            
+            r++;
+            
+            ans+=r-l+1;
+        
+            
+        }
+        
+        return ans;
+    }
+    
     int countSubstr(string& s, int k) {
-        // code here.
-        return f(s,k)-f(s,k-1);
+        // code here
+        return solve(s,k)-solve(s,k-1);
+        
     }
 };
