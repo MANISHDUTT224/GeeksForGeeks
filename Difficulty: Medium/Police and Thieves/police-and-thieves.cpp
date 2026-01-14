42e738044d134538973f2096b990bb6f0701594d
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-01-14 09:43:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
@@ -2,34 +2,29 @@ class Solution {
   public:
     int catchThieves(vector<char> &arr, int k) {
         // Code here
-        deque<int>thief_q,police_q;
-        int cc=0;
-        for(int i=0;i<arr.size();i++){
+         int i=0,j=1,n=arr.size();
+        int count=0;
+        while(i<n&&j<n){
             if(arr[i]=='P'){
-                while(!thief_q.empty() && thief_q.front()<i-k){
-                    thief_q.pop_front();
-                }
-                if(!thief_q.empty()){
-                    thief_q.pop_front();
-                    cc++;
-                }
-                else{
-                    police_q.push_back(i);
+                while(j<n&&arr[j]!='T')j++;
+                if(j<n&&j-i<=k){
+                    arr[i]='C';
+                    arr[j]='C';
+                    count++;
+                    j++;
                 }
             }
             else if(arr[i]=='T'){
-                while(!police_q.empty() && police_q.front()<i-k){
-                    police_q.pop_front();
-                }
-                if(!police_q.empty()){
-                    police_q.pop_front();
-                    cc++;
-                }
-                else{
-                    thief_q.push_back(i);
+                while(j<n&&arr[j]!='P')j++;
+                if(j<n&&j-i<=k){
+                    arr[i]='C';
+                    arr[j]='C';
+                    count++;
+                    j++;
                 }
             }
+            i++;
         }
-        return cc;
+        return count;
     }
 };
