2be8544ddc93636eff2c0e573dd54a04ad464e31
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-01-15 06:37:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Candy/candy.cpp
+++ Difficulty: Hard/Candy/candy.cpp
@@ -2,21 +2,19 @@ class Solution {
   public:
     int minCandy(vector<int> &arr) {
         // code here
-        int cnt=0;
         int n=arr.size();
-        
-        vector<int>candies(n,1);
-        for(int i=1;i<n;i++){
-            if(arr[i]>arr[i-1]){
-                candies[i]=max(candies[i],candies[i-1]+1);
-            }
-        }
-        for(int i=n-1;i>0;i--){
-            if(arr[i-1]>arr[i]){
-                candies[i-1]=max(candies[i]+1,candies[i-1]);
-            }
-            cnt+=candies[i-1];
-        }
-        return cnt+candies[n-1];
+      vector<int> lmax(n, 1), rmax(n, 1);
+      
+      for(int i=1; i<n; i++){
+        if(arr[i] > arr[i-1]) lmax[i]=1+lmax[i-1];      
+      }      
+      for(int i=n-2; i>=0; i--){
+        if(arr[i] > arr[i+1]) rmax[i]=1+rmax[i+1];      
+      }
+      
+      int ans=0;
+      for(int i=0; i<n; i++) ans+=max(lmax[i], rmax[i]);
+      
+      return ans;
     }
 };
