5d89cfd50f2a220a349a441a53a6955ac6621cfe
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-01-18 08:06:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
@@ -1,34 +1,23 @@
 class Solution {
   public:
-    vector<int> findGreater(vector<int>& arr) {
+    vector<int> nextFreqGreater(vector<int>& arr) {
         // code here
-        unordered_map<int,int>mp;
-        for(auto i:arr){
-            mp[i]++;
+        unordered_map<int,int>m;
+        for(int i=0;i<arr.size();i++){
+            m[arr[i]]++;
         }
-        vector<int>tempans(arr.size(),-1);
-        stack<int>st;
-        st.push(arr[tempans.size()-1]);
-        for(int i=arr.size()-2;i>=0;i--){
-            if(mp[arr[i]]<mp[st.top()]){
-                tempans[i]=st.top();
-                st.push(arr[i]);
-            }
-            else{
-                while(!st.empty()&&mp[st.top()]<=mp[arr[i]]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    tempans[i]=-1; 
-                st.push(arr[i]);
-                }
-                else{
-                     tempans[i]=st.top();
-                st.push(arr[i]);
-                }
-            }
+        vector<int>ans;
+        stack<pair<int,int>>s;
+        for(int i=arr.size()-1;i>=0;i--){
+            while(!s.empty() && s.top().second<=m[arr[i]])
+            s.pop();
+            if(!s.empty())
+            ans.push_back(s.top().first);
+            else
+            ans.push_back(-1);
+            s.push({arr[i],m[arr[i]]});
         }
-        
-      return tempans;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
