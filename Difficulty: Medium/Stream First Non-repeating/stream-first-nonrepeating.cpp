383b879295b1cbaeeb0517afe8691364a28e080a
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-01-30 07:43:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.cpp
+++ Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.cpp
@@ -2,15 +2,22 @@ class Solution {
   public:
     string firstNonRepeating(string &s) {
         // code here
-        vector<int> count(26,0);
-        queue<char> q;
-        string ans="";
-        for(auto i:s){
-            count[i-'a']++;
-            if(count[i-'a']==1)q.push(i);
-            while(!q.empty()&&count[q.front()-'a']>1)q.pop();
-            if(q.empty())ans+='#';
-            else ans+=q.front();
+       queue<char>q;
+        map<char,int>mp;
+        int n=s.size();
+        string ans;
+        for(int i=0;i<n;i++){
+            char x=s[i];
+            if(mp.find(x)==mp.end()){
+                q.push(x);
+                mp[x]++;
+            } else {
+                mp[x]++;
+                while(!q.empty() && mp[q.front()]>1){
+                    q.pop();
+                }
+            }
+            ans.push_back(q.empty() ? '#' : q.front());
         }
         return ans;
     }
