6b759df019737a234eae2f9a53051c261301ba07
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-02-05 07:52:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
@@ -2,47 +2,20 @@ class Solution {
   public:
     int maxOnes(vector<int>& arr, int k) {
         // code here
-         stack<int> st;
-        vector<int> v;
-        int cnt = 0, ans = 0, temp = 0;
-
-        for (int i = 0, j = arr.size() - 1; i < arr.size(); i++, j--) {
-            if (arr[j] == 0) {
-                st.push(j);
-            }
-
-            if (arr[i] == 1) {
-                cnt++;
-            }
-
-            v.push_back(cnt);
-
-            if (arr[i] == 0) {
-
-                cnt = 0;
-            }
+        int maxi=0, c0=0, i=0, j=0;
+      
+      int n=arr.size();
+      
+      for(j=0; j<n; j++){
+        if(arr[j]==0) c0++;
+        while(c0>k){
+          if(arr[i]==0) c0--;
+          i++;
         }
-
-        for (int i = 0; i < arr.size(); i++) {
-
-            if (arr[i] == 0 && k == 0) {
-                temp = temp - v[st.top()];
-
-                st.pop();
-                ans = max(ans, temp);
-
-            }
-
-            else {
-                if (arr[i] == 0) {
-                    k--;
-                }
-
-                temp = temp + 1;
-                ans = max(ans, temp);
-            }
-        }
-
-        return ans;
+        
+        maxi = max(maxi, j-i+1);
+      }
+      
+      return maxi;
     }
 };
