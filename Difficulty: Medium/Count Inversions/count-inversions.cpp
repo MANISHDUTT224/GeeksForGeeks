d158a6a9d257ca56a2a09e593c0f2caa37aadace
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-02-18 07:54:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -1,48 +1,60 @@
 class Solution {
   public:
-    // Function to count inversions in the array.
-    int merge(vector<int>&arr,int l,int mid,int r){
-        int i=l,j=mid+1;
-        int cnt=0;
-        vector<int>temp;
-        while(i<=mid && j<=r){
-            if(arr[i]<=arr[j]){
-                temp.push_back(arr[i]);
-                i++;
-            }
-            else{
-                cnt+=(mid-i+1);
-                temp.push_back(arr[j]);
-                j++;
-            }
-        }
-        while(i<=mid){
-            temp.push_back(arr[i]);
-            i++;
-        }
-        while(j<=r){
-            temp.push_back(arr[j]);
-            j++;
-        }
-        int k=temp.size();
-        for(int i=l;i<=r;i++){
-            arr[i]=temp[i-l];
-        }
-        return cnt;
+    virtual int merge(int s, int e, int m, vector<int> &arr){
+      
+      int ln=m-s+1, rn=e-m;
+      vector<int> left(ln);
+      vector<int> right(rn);
+      
+      int k=s;
+      while(k<=m){
+        left[k-s]=arr[k]; k++;  
+      }
+      
+      k=m+1;
+      while(k<=e){
+        right[k-m-1]=arr[k]; k++;  
+      }
+      
+      k=s;
+      int ans=0;
+      int l=0, r=0;
+      
+      while(l<ln && r<rn){
+        if(left[l] <= right[r]){
+          arr[k++] = left[l++];    
+        }else{
+          arr[k++] = right[r++];
+          ans += (ln-l);
+        }  
+      }
+      
+      while(l<ln){
+        arr[k++] = left[l++];  
+      }
+      
+      while(r<rn){
+        arr[k++] = right[r++];  
+      }
+      
+      return ans;      
     }
-    int mergeSort(vector<int>&arr,int l,int r){
-        int cnt=0;
-        if(l>=r){
-            return cnt;
-        }
-        int mid=(l+r)/2;
-        cnt+=mergeSort(arr,l,mid);
-        cnt+=mergeSort(arr,mid+1,r);
-        cnt+=merge(arr,l,mid,r);
-        return cnt;
+    
+    
+    virtual int mergeSort(int s, int e, vector<int> &arr){
+      if(s>=e) return 0;
+      int ans=0;
+      int m=s+(e-s)/2;
+      
+      ans += mergeSort(s, m, arr);
+      ans += mergeSort(m+1, e, arr);
+      ans += merge(s, e, m, arr);
+      
+      return ans;
     }
-    int inversionCount(vector<int> &arr) {
-        // Your Code Here
-        return mergeSort(arr,0,arr.size()-1);
+    
+    virtual int inversionCount(vector<int> &arr) {
+      int n=arr.size();
+      return mergeSort(0, n-1, arr);
     }
 };
