21ed649979ee7eae9b8fa8118a27a0beb996b133
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-02-21 09:14:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,14 +1,23 @@
 class Solution {
   public:
-    int hIndex(vector<int>& citations) {
+    int hIndex(vector<int>& c) {
         // code here
-         sort(citations.begin(),citations.end());
-        int i=citations.size();
-        int j=0;
-        while(i>citations[j]){
-            i--;
-            j++;
+         int n = c.size();
+        
+        sort(c.begin(), c.end());
+        
+        int l = 0, r = n, ans = -1;
+        
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(c[n-mid] >= mid) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
-        return i;
+        
+        return ans;
     }
 };
