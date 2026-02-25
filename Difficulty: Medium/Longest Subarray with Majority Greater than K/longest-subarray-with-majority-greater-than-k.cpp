dd776f40aa8b06dedcfd36b869e4b3a9a519b4b5
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-02-25 21:26:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -2,29 +2,18 @@ class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
         // Code here
-        int n=(int)arr.size();
-        int res=0;
+         int n = arr.size(), ans = 0, ct = 0;
+        unordered_map<int, int> mp;
         
-        map<int,int>mp;
-        mp[0]=-1;
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]>k)
-            sum++;
-            else
-            sum--;
+        for(int i=0; i<n; i++) {
+            if(arr[i]>k) ct++;
+            else ct--;
             
-            if(mp.find(sum)==mp.end())
-            mp[sum]=i;
+            if(ct > 0) ans = max(ans, i+1);
+            else if(mp.find(ct-1)!=mp.end()) ans = max(ans, i-mp[ct-1]);
             
-            if(mp.find(sum-1)!=mp.end())
-            res=max(res,i-mp[sum-1]);
-            
-            if(sum>0)
-            res=max(res,i+1);
-            
+            if(mp.find(ct)==mp.end()) mp[ct]=i;
         }
-        return res;
+        return ans;
     }
 };
