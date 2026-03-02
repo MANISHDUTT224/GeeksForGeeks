e1b8c1ca849076b05c858dcf0f635ffc5264be8a
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-03-02 08:31:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -2,21 +2,23 @@ class Solution {
   public:
     int maxWater(vector<int> &arr) {
         // code here
-        int n=arr.size();
-        vector<int>left(n,0);
-        vector<int>right(n,0);
-        int trapped=0;
-        left[0]=arr[0],right[n-1]=arr[n-1];
-        for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],arr[i]);
+        int left=0,right=arr.size()-1;
+        int leftMax=arr[left],rightMax=arr[right];
+        int water=0;
+        while(left<right){
+            if(leftMax<rightMax){
+                left++;
+                leftMax=max(leftMax,arr[left]);
+                water+=leftMax-arr[left];
+                
+            }
+            else{
+                right--;
+                rightMax=max(rightMax,arr[right]);
+                water+=rightMax-arr[right];
+                
+            }
         }
-        for(int i=n-2;i>=0;i--){
-            right[i]=max(right[i+1],arr[i]);
-        }
-        for(int i=0;i<n;i++){
-            int waterlevel=min(left[i],right[i]);
-            trapped+=(waterlevel-arr[i])*1;
-        }
-        return trapped;
+        return water;
     }
 };
