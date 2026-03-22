a1f076eab7ee300112e309c41da13a3fa67fd6ae
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-03-22 11:23:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
@@ -1,80 +1,51 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  public:
-  int dx[4]={-1,0,1,0};
-  int dy[4]={0,-1,0,1};
-  bool isValid(int i,int j,int n,int m,vector<vector<int>>&mat){
-      if(i>=0 && j>=0 && i<n && j<m && mat[i][j]==1){
-          return true;
-      }
-      return false;
-  }
-    int orangesRotting(vector<vector<int>>& mat) {
-        // Code here
-        queue<pair<int,pair<int,int>>>q;
-        int n=mat.size(),m=mat[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==2){
-                    q.push({i,{j,0}});
-                }
-            }
-        }
-        int ans=0;
-        while(!q.empty()){
-            auto z=q.front();
-            int ix=z.first;
-            int jy=z.second.first;
-            int cost=z.second.second;
-            ans=max(ans,cost);
-            q.pop();
-            for(int k=0;k<4;k++){
-                int row=ix+dx[k];
-                int col=jy+dy[k];
-                if(isValid(row,col,n,m,mat)){
-                    q.push({row,{col,cost+1}});
-                    mat[row][col]=2;
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-        return ans;
-    }
-};
+public:
+    int orangesRot(vector<vector<int>>& grid) {
+        if (grid.empty()) return 0;
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, int>> q;
+        int freshCount = 0;
 
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> mat(n, vector<int>(m, -1));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                cin >> mat[i][j];
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    freshCount++;
+                }
             }
         }
-        Solution obj;
-        int ans = obj.orangesRotting(mat);
-        cout << ans << "\n";
+    
+        if (freshCount == 0) return 0;
+        
+        int minutes = 0;
 
-        cout << "~"
-             << "\n";
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+ 
+        while (!q.empty() && freshCount > 0) {
+            int size = q.size();
+            
+            for (int i = 0; i < size; i++) {
+                auto [r, c] = q.front();
+                q.pop();
+            
+                for (auto& dir : dirs) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+                    
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2; 
+                        q.push({nr, nc}); 
+                        freshCount--;     
+                    }
+                }
+            }
+            minutes++;
+        }
+        
+        return freshCount == 0 ? minutes : -1;
     }
-    return 0;
-}
-// } Driver Code Ends
+};
