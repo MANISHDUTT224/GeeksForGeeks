0452d4d20c944dbffc9d19fcbc79f5eb143f32ca
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-04-11 06:38:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorted subsequence of size 3/sorted-subsequence-of-size-3.cpp
+++ Difficulty: Medium/Sorted subsequence of size 3/sorted-subsequence-of-size-3.cpp
@@ -1,28 +1,25 @@
 class Solution {
   public:
     vector<int> find3Numbers(vector<int> &arr) {
-        int n = arr.size();
+        // Code here
+             int n=arr.size();
+        vector<int>small(n, 1e9), big(n, -1e9);
         
-        if (n < 3) return {};
-        
-        vector<int> rightMax(n);
-        rightMax[n - 1] = arr[n - 1];
-        
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = max(arr[i], rightMax[i + 1]);
+        small[0]=arr[0];
+        for(int i=1; i<n; i++){
+            small[i]=min(small[i-1], arr[i]);
         }
         
-        int leftMin = arr[0];
+        big[n-1]=arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            big[i]=max(big[i+1], arr[i]);
+        }
         
-        for (int i = 1; i < n - 1; i++) {
-            
-            if (leftMin < arr[i] && arr[i] < rightMax[i + 1]) {
-                return {leftMin, arr[i], rightMax[i + 1]};
+        for(int i=0; i<n; i++){
+            if(arr[i]>small[i] && arr[i]<big[i]){
+                return {small[i], arr[i], big[i]};
             }
-            
-            leftMin = min(leftMin, arr[i]);
         }
-        
         return {};
     }
 };
