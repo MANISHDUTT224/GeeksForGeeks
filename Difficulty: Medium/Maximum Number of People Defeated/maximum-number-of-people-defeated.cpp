529fa6f11f78a330399668def44fa7d3d897b10a
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-06-23 07:00:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+ int maxPeopleDefeated(int p) {
+        int low=1;
+        int high=1000;
+        int ans=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            long long x=(mid*(mid+1)*(2*mid+1))/6;
+            if(x>p)
+            high=mid-1;
+            else
+            {
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }};
